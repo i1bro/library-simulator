9fcc1bba23c3b97a509820cc4510ebf51a539c51
Ilya Brovarnik <ilya.brovarnik@gmail.com>
2022-10-30 19:14:08 +0100

    init

---
+++ src/main/kotlin/com/example/librarysimulator/LibrarySimulatorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.librarysimulator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibrarySimulatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<LibrarySimulatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/librarysimulator/LibrarySimulatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.librarysimulator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibrarySimulatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
